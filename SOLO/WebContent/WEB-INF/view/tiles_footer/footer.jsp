da07c9721ed3ae7a5654f93cf41e58c58df71a9b
jhbb7 <jhbb7@DESKTOP-67FRLDN>
2017-08-27 23:53:47 +0900

    중간

---
+++ SOLO/WebContent/WEB-INF/view/tiles_footer/footer.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+</head>
+<body>
+<h2>발</h2>
+</body>
+</html>
---
+++ SOLO/src/main/controller/main.java
@@ -0,0 +1,23 @@
+package main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class main {
+	@RequestMapping("/")
+	public ModelAndView home() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("t:mainview/main");			
+		return mav;
+	}
+	
+	@RequestMapping("/start")
+	public ModelAndView home2() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("p:header/body/footer");			
+		return mav;
+	}
+
+}
